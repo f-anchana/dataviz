139fbe5b778324bee4cb458e45def3fbdaccb09e
Anchana <fatimarajananchana@gmail.com>
2023-10-18 15:40:11 +0200

    indentation

--- script.js
+++ script.js
@@ -14,25 +14,25 @@ d3.json("rapdata.json").then(function (data) {
 
     d3.selectAll(".histobarre")
         .append("rect")
-        .attr("width", largeur_baton)   
+        .attr("width", largeur_baton)
         .attr("height", (d) => d.Nbr * 100 / 2000000000)
         .attr("fill", (d, i) => couleur[i % couleur.length])
         .attr("transform", `scale(1,-1)`)
 
-        
+
 
     d3.selectAll('.histobarre')
-        .on("mouseenter",function(e,d){
+        .on("mouseenter", function (e, d) {
             d3.selectAll('.histobarre')
-                .style("opacity",0.5)
-            d3.select( this )
-                .style("opacity",1)
+                .style("opacity", 0.5)
+            d3.select(this)
+                .style("opacity", 1)
         })
-        .on("mouseleave",function(e,d){
+        .on("mouseleave", function (e, d) {
             d3.selectAll('.histobarre')
-                .style("opacity",1)
+                .style("opacity", 1)
         })
-        .on("mouseclick",function(e,d){
+        .on("mouseclick", function (e, d) {
         })
 })
 
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
         response.json().then(function (data) {
             data.forEach(function afficherap(items) {
                 var liste = document.querySelector(".chanteurs");
-                liste.innerHTML = liste.innerHTML + "<section class='rap' id=" + items.id + "><img src='" + items["img"] + "'class='image'><h1> Winner : " + items.year + " </h1><h2>" + items.titre +" by <span class='artista'>"+ items.artiste+"</span></h2></section>";
+                liste.innerHTML = liste.innerHTML + "<section class='rap' id=" + items.id + "><img src='" + items["img"] + "'class='image'><h1> Winner : " + items.year + " </h1><h2>" + items.titre + " by <span class='artista'>" + items.artiste + "</span></h2></section>";
 
             });
 

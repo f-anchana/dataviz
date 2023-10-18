f7da8b0df6d9d0ebe57df4304c2cf8fb1c74fa67
offkrn <124247628+Krxrn@users.noreply.github.com>
2023-10-18 11:12:10 +0200

    data json

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ d3.json("rapdata.json", function (data){
     
 })
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     fetch('rapdata.json').then(function (response) {
-//         response.json().then(function (data) {
-//             data.forEach(function afficheAnalogie(items) {
-//                 var liste = document.querySelector(".chanteurs");
-//                 liste.innerHTML = liste.innerHTML + "<section id=" + items.id + "><img src='" + items["image"] + "'class='imag'><div class='content'><h2>Si j’étais " + items.analogie + ", je serais " + items.valeurAnalogie + ".</h2><p>" + items.explication + "</p></div></section>";
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('rapdata.json').then(function (response) {
+        response.json().then(function (data) {
+            data.forEach(function afficherap(items) {
+                var liste = document.querySelector(".chanteurs");
+                liste.innerHTML = liste.innerHTML + "<section id=" + items.id + "><img src='" + items["img"] + "'class='image'><p> Winner : " + items.year + " </p><h1></h1></section>";
 
-//             });
+            });
 
 
             // Popup
@@ -29,7 +29,7 @@ d3.json("rapdata.json", function (data){
 
 
 
-//         });
+        });
 
-//     })
-// });
+    })
+});
